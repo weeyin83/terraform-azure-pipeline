79abd17cd64a289d48939e0f4635122afa2c7ecc
Sarah Lean <sarahlean@microsoft.com>
2025-07-16 13:14:09 +0100

    update terraform

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">= 2.2.0, < 3.0.0"
+    }
   }
 }
 
@@ -11,18 +15,11 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "resource_group" {
-  name     = "rg-acs-deployment"
-  location = var.location
+provider "azapi" {
+  # Configuration options
 }
 
-# This ensures we have unique CAF compliant names for our resources.
-module "naming" {
-  source  = "Azure/naming/azurerm"
-  version = "0.3.0"
-}
-
-# This is required for resource modules
+# This is the deployment of the Azure resource group. 
 resource "azurerm_resource_group" "rg" {
   location = var.location
   name     = module.naming.resource_group.name_unique
@@ -34,10 +31,26 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
 
+#This is the module that deploys a custom DNS zone. 
 module "avm-res-network-dnszone" {
   source              = "Azure/avm-res-network-dnszone/azurerm"
   version             = "0.1.0"
   name                = var.custom_domain
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Deploy Log Analytics Workspace
+module "avm-res-operationalinsights-workspace" {
+  source  = "Azure/avm-res-operationalinsights-workspace/azurerm"
+  version = "0.4.2"
+  # insert the 3 required variables here
+  name                = "log-analytics-workspace"
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
